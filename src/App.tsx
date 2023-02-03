82a26a110de156346023c2351f47cc400146e9cc
MaahiVohra <vohramaahi@gmail.com>
2023-02-03 15:19:18 +0530

    feature: added dynamic text

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
-
+import { getWords } from "./words";
+const NO_OF_WORDS = 10;
 function App() {
-	// converts the text string to an array to use the map function
-	var originalText = "this is a letter";
-	const originalTextArray = originalText.split("");
+	const [wordloader, setWordloader] = useState(true);
+	var textArray: string[] = useMemo(
+		() => getWords(NO_OF_WORDS),
+		[wordloader]
+	);
+	const [textonScreen, setTextonScreen] = useState<String[]>([]);
+	useEffect(() => {
+		setTextonScreen(textArray);
+	}, []);
+	async function chooseRandomWords() {
+		await setWordloader((prev) => !prev);
+		setCorrectLetters([]);
+		setCorrectLetters([]);
+		setCurrentLetterIndex(0);
+		setTextonScreen(textArray);
+	}
 
 	const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
 	const [correctLetters, setCorrectLetters] = useState<Number[]>([]);
@@ -18,23 +32,25 @@ function App() {
 	//controls the input key press
 	function controller(e: any) {
 		e.preventDefault();
-		console.log(e.key);
-		var move = 1;
-		if (e.key === originalText[currentLetterIndex]) {
-			setCorrectLetters((prev) => {
-				return [...prev, currentLetterIndex];
-			});
+		if (e.key.length === 1) {
+			if (e.key === textArray[currentLetterIndex]) {
+				setCorrectLetters((prev) => {
+					return [...prev, currentLetterIndex];
+				});
+			}
+			setCurrentLetterIndex((prev) => prev + 1);
+			return;
 		}
 		if (e.key === "Backspace") {
-			move = -1;
 			setCorrectLetters((prev) => {
 				return [...prev].filter(
 					(letter) => letter !== currentLetterIndex - 1
 				);
 			});
+			if (currentLetterIndex !== 0)
+				setCurrentLetterIndex((prev) => prev - 1);
+			return;
 		}
-		if (currentLetterIndex + move < 0) move = 0;
-		setCurrentLetterIndex((prev) => prev + move);
 		return true;
 	}
 
@@ -44,23 +60,34 @@ function App() {
 		if (index >= currentLetterIndex) return "gray";
 		else return "yellow";
 	}
-
+	function showstuff(e: any = null) {
+		console.log("	");
+		console.log(currentLetterIndex);
+		console.log(textArray);
+		console.log(e);
+		console.log("	");
+	}
 	return (
 		<div className="App" onKeyDown={controller} tabIndex={1} ref={inputRef}>
 			<span className="cursor">|</span>
-			{originalTextArray.map((text, index: number) => {
-				return (
-					<span
-						style={{ color: getColor(index) }}
-						className={
-							currentLetterIndex === index ? "active" : undefined
-						}
-						key={index}
-					>
-						{text}
-					</span>
-				);
-			})}
+			{textonScreen &&
+				textArray.map((text, index: number) => {
+					return (
+						<span
+							style={{ color: getColor(index) }}
+							className={
+								currentLetterIndex === index
+									? "active"
+									: undefined
+							}
+							key={index}
+						>
+							{text}
+						</span>
+					);
+				})}
+			<button onClick={chooseRandomWords}>reset</button>
+			<button onClick={showstuff}>show</button>
 		</div>
 	);
 }
